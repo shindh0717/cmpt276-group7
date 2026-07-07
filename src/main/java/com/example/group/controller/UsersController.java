35d9ba90dd6d0efb804ed6b34e968facbc85ccb8
Aveer100 <ass96@sfu.ca>
2026-07-06 19:42:00 -0700

    Update UsersController.java
    fixed logout placement

--- src/main/java/com/example/group/controller/UsersController.java
+++ src/main/java/com/example/group/controller/UsersController.java
@@ -3,16 +3,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class UsersController {
-    @GetMapping("/logout")
-    public String destroySession(HttpServletRequest request, HttpServletResponse response){
-        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-        response.setHeader("Pragma", "no-cache");
-        response.setHeader("Expires", "0");
-        request.getSession().invalidate();
-        return "redirect:/login";
-    }
-
 import java.util.List;
 import java.util.Optional;
 
@@ -96,6 +86,15 @@ public class UsersController {
         }
 
        
+    }
+
+    @GetMapping("/logout")
+    public String destroySession(HttpServletRequest request, HttpServletResponse response){
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Expires", "0");
+        request.getSession().invalidate();
+        return "redirect:/login";
     }
 
     @Autowired
